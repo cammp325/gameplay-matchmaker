4d455fd682b012a6171d02d0d8170426f9196d98
Ayo <alawani@students.kennesaw.edu>
2023-03-12 17:20:26 -0400

    Fix testing config files

--- client/cypress/e2e/spec.cy.ts
+++ client/cypress/e2e/spec.cy.ts
@@ -1,5 +1,11 @@
-describe('template spec', () => {
+/* describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
+  })
+}) */
+
+describe('spec.cy.ts', () => {
+  it('should visit', () => {
+    cy.visit('https://localhost:4200/')
   })
 })
