e60762aca74c2d274a3e42698f1913062e8f9a8c
Ayo <alawani@students.kennesaw.edu>
2023-03-12 16:12:18 -0400

    Add Cypress e2e test and Jasmine unit tests

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,12 +1,20 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+
 import { AppComponent } from './app.component';
 
+
 describe('AppComponent', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientTestingModule
       ],
       declarations: [
         AppComponent
@@ -26,10 +34,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('Gameplay Matchmaker');
   });
 
-/*   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('client app is running!');
-  }); */
+  /*   it('should render title', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+      const compiled = fixture.nativeElement as HTMLElement;
+      expect(compiled.querySelector('.content span')?.textContent).toContain('client app is running!');
+    }); */
 });
